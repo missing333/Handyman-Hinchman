738ee775197a1c4fe1b90a62c49da656605da919
Marc <mmissing3@gmail.com>
2025-03-19 21:44:43 -0700

    install terser

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,8 +16,9 @@ export default defineConfig({
       minify: 'terser',
       terserOptions: {
         compress: {
-          drop_console: true
-        }
+          drop_console: true,
+          drop_debugger: true,
+        },
       },
       rollupOptions: {
         output: {
